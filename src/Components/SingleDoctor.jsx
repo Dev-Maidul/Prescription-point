b33afaecd182aeab70abcb47b47b01d080ee97a1
Dev-Maidu <maidulislammanik8991@gmail.com>
2025-06-11 12:17:25 +0800

    add show all and less functionality

--- src/Components/SingleDoctor.jsx
+++ src/Components/SingleDoctor.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { RiRegisteredLine } from "react-icons/ri";
 
 const SingleDoctor = ({ doctor }) => {
   const {
@@ -10,25 +11,33 @@ const SingleDoctor = ({ doctor }) => {
     education,
   } = doctor;
   return (
-    <div className="card bg-base-100 w-96 shadow-sm p-6">
+    <div className="card bg-base-100 shadow-sm p-6">
       <figure>
         <img
-          className="w-[350px] h-[250px] rounded-2xl"
+          className="w-full h-[250px] rounded-2xl"
           src={image}
           alt="Doctor Photo"
         />
       </figure>
       <div className="flex gap-2 items-center py-4">
-        <button className="btn btn-outline btn-accent">{availability && <p>Available</p>}</button>
-        <button className="btn btn-outline btn-info">{experience} experience</button>
+        <button className="btn btn-outline btn-accent">
+          {availability && <p>Available</p>}
+        </button>
+        <button className="btn btn-outline btn-info">
+          {experience} experience
+        </button>
       </div>
       <div className="card-body">
-        <h2 className="card-title">{name}</h2>
-        <p className="text-left">Degree: {education}</p>
-        <div className="card-actions justify-end">
-          <div className="badge badge-outline">Fashion</div>
-          <div className="badge badge-outline">Products</div>
+        <div className="border-b border-dashed border-gray-400 pb-4 mb-4">
+          <h2 className="card-title text-2xl">{name}</h2>
+          <p className="text-left text-xl text-gray-500">{education}</p>
         </div>
+
+        <div className="card-actions justify-between">
+          <h1 className="flex text-xl gap-1 items-center"><RiRegisteredLine size={26}/>Reg No: {registration_number} </h1>
+          
+        </div>
+        <button className="btn btn-outline btn-primary text-xl font-bold mt-2">View Details</button>
       </div>
     </div>
   );
--- src/Pages/Home/Doctors.jsx
+++ src/Pages/Home/Doctors.jsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SingleDoctor from '../../Components/SingleDoctor';
 
 const Doctors = ({doctors}) => {
+    const [displayDoctors,setDisplayDoctors]=useState([]);
+    const [showAll,setShowAll]=useState(false);
+
+    useEffect(()=>{
+        if(showAll)
+        {
+            setDisplayDoctors(doctors);
+        }
+        else{
+            setDisplayDoctors(doctors.slice(0,6));
+        }
+    },[doctors,showAll]);
     console.log(doctors);
     return (
-        <div className='w-10/12 mx-auto text-center py-8'>
+        <div className='w-10/12 mx-auto text-center py-12 mt-12'>
             <h1 className='font-bold text-3xl text-center'>Our Best Doctors</h1>
             <p className='text-center font-thin'>Our platform connects you with verified, experienced doctors across various specialties — all at your convenience. Whether it's a <br />routine checkup or urgent consultation, book appointments in minutes and receive quality care you can trust.</p>
-            <div className="doctor-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-2"> 
+            <div className="doctor-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-2 py-12"> 
                 {
-                    doctors.map((doctor)=> <SingleDoctor doctor={doctor} key={doctor.id}></SingleDoctor>)
+                    displayDoctors.map((doctor)=> <SingleDoctor doctor={doctor} key={doctor.id}></SingleDoctor>)
                 }
             </div>
+            <button onClick={()=> {
+                setShowAll(prev=>!prev);
+                if(showAll) window.scrollTo(0,400);
+            }} className="relative inline-block text-lg group cursor-pointer text-center mx-auto">
+    <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+        <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+        <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+        <span className="relative">{showAll? 'Show Less':"Show All"}</span>
+    </span>
+    <span className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+</button>
         </div>
         
     );
